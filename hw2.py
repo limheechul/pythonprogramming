9140c8595e66357c82df0b78cf0c044e3f09084c
limheechul <93422840+limheechul@users.noreply.github.com>
2025-03-23 14:22:23 +0900

    Create hw2.py

---
+++ hw2.py
@@ -0,0 +1,17 @@
+def get_integer():
+    amount = int(input("동전으로 교환하고자 하는 금액은? "))
+    return amount
+
+def exchange(amount):
+    coins = [500, 100, 50, 10]
+    coin_counts = {}
+
+    for coin in coins:
+        coin_counts[coin] = amount // coin
+        amount %= coin
+
+    for coin in coins:
+        print(f"{coin}원 동전의 개수: {coin_counts[coin]}")
+
+amount = get_integer()
+exchange(amount)
