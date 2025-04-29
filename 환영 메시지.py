fd9a70bf4ecf25e5f820c9fd5c5fed5a0c394751
limheechul <93422840+limheechul@users.noreply.github.com>
2025-04-29 21:08:23 +0900

    Delete 환영 메시지.py

--- 환영 메시지.py
+++
@@ -1,9 +0,0 @@
-def draw_line_string(n):
-    msg1 = 'hello' + n
-    msg2 = "Welcome to seoul."
-    nstr = len(msg1) if (len(msg1) > len(msg2)) else len(msg2)
-    print('-'*nstr)
-    print('Hello', n+',\n'+msg2)
-    print('-'*nstr)
-
-draw_line_string(input('input his/her name:'))
