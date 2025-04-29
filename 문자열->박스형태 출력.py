6614b89e9c9b4da65146abb49b2234810f6cd408
limheechul <93422840+limheechul@users.noreply.github.com>
2025-04-29 21:02:37 +0900

    Delete 문자열->박스형태 출력.py

--- 문자열->박스형태 출력.py
+++
@@ -1,8 +0,0 @@
-def draw_line_string(msg):
-    nstr = len(msg)
-    rep_char('-', nstr * 2 + 4)
-    print(f'  {msg}  ')
-    rep_char('-', nstr * 2 + 4)
-
-draw_line_string('안녕하세요')
-draw_line_string('안녕')
