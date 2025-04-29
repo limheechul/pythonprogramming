6d8b399d221e876620e31026168ee51635cfa18d
limheechul <93422840+limheechul@users.noreply.github.com>
2025-04-29 21:03:13 +0900

    Delete 나이 유효성 검사.py

--- 나이 유효성 검사.py
+++
@@ -1,7 +0,0 @@
-age = int(input('당신의 나이는? '))
-res = 0 <= age <= 120
-print(f'{age}은(는) 유효한 나이인가? {res}')
-
-res = age < 0 or age > 120
-print(f'{age}은(는) 유효한 나이가 아닌가? {res}')
-print(f'{age}은(는) 유효한 나이인가? {not res}')
