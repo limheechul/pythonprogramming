4fe50aeb1fd21d97d92fd850a8cf4adca9120d33
limheechul <93422840+limheechul@users.noreply.github.com>
2025-04-29 21:05:43 +0900

    Delete 이름,학년->문자열로 반환.py

--- 이름,학년->문자열로 반환.py
+++
@@ -1,9 +0,0 @@
-def introduce(name, grade):
-    first_name = name[1:]
-    next_grade = grade + 1
-    return f"{first_name}은 내년에 {next_grade}학년입니다."
-
-name = input("이름? ")
-grade = int(input("학년? "))
-
-print(introduce(name, grade))
