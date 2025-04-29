f6354ed6461d2f36772a649bf0f830142e8bb6ea
limheechul <93422840+limheechul@users.noreply.github.com>
2025-04-29 21:01:57 +0900

    Delete 2~9단 구구단 나열.py

--- 2~9단 구구단 나열.py
+++
@@ -1,9 +0,0 @@
-def print_m_table(start, end):
-    for i in range(1,10):
-        for dan in range(start, end + 1):
-            print(f'{dan} x {i} = {dan * i:2d}', end = '\t')
-        print()
-    
-print_m_table(2,5)
-print()
-print_m_table(6,9)
