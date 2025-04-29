8764293df872f225c14815fd821df709f05d8c23
limheechul <93422840+limheechul@users.noreply.github.com>
2025-04-29 21:02:20 +0900

    Delete n비트를 10진수(2진수)로 표현.py

--- n비트를 10진수(2진수)로 표현.py
+++
@@ -1,9 +0,0 @@
-def set_all_bits(n):
-    return (1 << n) - 1
-
-def get_integer(prompt):
-    return int(input(prompt))
-
-n = get_integer('설정할 비트 수는?')
-all_ones = set_all_bits(n)
-print(n, '비트를 모두 1로 설정한 수는', all_ones,'(',bin(all_ones), ')이다')
